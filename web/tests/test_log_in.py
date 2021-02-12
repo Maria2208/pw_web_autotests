ef03182e5157985b97b9cddeaa34cb66c6246ad3
Maria Tkachenko <maria.btm22@gmail.com>
2021-02-12 17:06:31 +0300

    add new test for test_log_in

--- web/tests/test_log_in.py
+++ web/tests/test_log_in.py
@@ -11,5 +11,12 @@ def test_login_with_valid_data(browser):
     time.sleep(5)
     assert browser.current_url == "https://market.proleum.pro/opinfo"
 
-def test_login_with_
+
+def test_login_failed(browser):
+    log_in_page = LogInPage(browser)
+    log_in_page.go_to_site()
+    log_in_page.set_user_inputs("abc", "abcde")
+    time.sleep(5)
+    error_message = "Введен неверный логин или пароль"
+    assert error_message in browser.find_element(*LogInLocators.invalid_data_message).text
 
